4eb174999d630aed33f9cd767beaeebf46820c9f
G00414253@atu.ie <g00414253@atu.ie>
2024-11-27 14:42:50 +0000

    Added feign and code to interact with PaymentClient microservice

---
+++ src/main/java/ie/atu/week11example/PersonRepository.java
@@ -0,0 +1,9 @@
+/*
+package ie.atu.week11example;
+
+import jakarta.persistence.Id;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PersonRepository extends JpaRepository<Person, Long> {
+}
+*/
--- src/main/java/ie/atu/week11example/PersonService.java
+++ src/main/java/ie/atu/week11example/PersonService.java
@@ -4,9 +4,22 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class PersonService {
+    private PaymentClient paymentClient;
+
+   // private PersonRepository personRepository;
+    //public PersonService(PersonRepository personRepository) {
+      //  this.personRepository = personRepository;
+    //}
+
+    public PersonService(PaymentClient paymentClient) {
+        this.paymentClient = paymentClient;
+    }
+
     // Placeholder method to save a person (add to Db in the next two weeks)
     public void savePerson(Person person) {
+      //  personRepository.save(person);
         System.out.println("Person saved: " + person);
+        String response = paymentClient.makePayment(person);
     }
 
     // Placeholder method to retrieve a person by employeeId
